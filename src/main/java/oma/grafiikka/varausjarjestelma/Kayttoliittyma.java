cd00f165432418e1631dbbbffc04605be1d3e177
eikka <eikka.kokkonen@icloud.com>
2024-04-25 11:30:33 +0300

    päivitettyä käyttöliittymää

--- src/main/java/oma/grafiikka/varausjarjestelma/Kayttoliittyma.java
+++ src/main/java/oma/grafiikka/varausjarjestelma/Kayttoliittyma.java
@@ -58,7 +58,163 @@ public class Kayttoliittyma extends Application {
         mokkienHallinta.setOnMouseClicked(e -> {
             mokkienHallinta();
         });
+        // Asiakkaiden hallinta ikkuna
+        asikkaidenHallinta.setOnAction(e -> {
+            asiakkaidenHallintaIkkuna();
+        });
+
+        // Palveluiden hallinta ikkuna
+        palveluidenHallinta.setOnAction(e -> {
+            palveluidenHallintaIkkuna();
+        });
     }
+
+    public void asiakkaidenHallintaIkkuna() {
+
+        // Luo uusi Borderpane
+
+        BorderPane pane = new BorderPane();
+        TextArea tiedot = new TextArea();
+        ListView<String> lista = new ListView<>();
+
+        // Luo uusi Stage-olio
+        Stage asiakasHallinta = new Stage();
+        Scene kehys = new Scene(pane);
+
+        // Luo buttonit ikkunaan
+        Button lisaaAsiakas = new Button("Lisää asiakas");
+        Button muokkaaAsiakas = new Button("Muokkaa asiakkaan tietoja");
+        Button poistaAsiakas = new Button("Poista asiakas");
+
+        // Luo VBox painikkeille ja aseta ne vasemmalle
+        VBox painikeVBox = new VBox(15);
+        painikeVBox.setPadding(new Insets(15, 15, 15, 15));
+        painikeVBox.getChildren().addAll(lisaaAsiakas, muokkaaAsiakas, poistaAsiakas);
+        pane.setRight(painikeVBox);
+
+        // Aseta TextArea ja ListView BorderPaneen
+        pane.setCenter(tiedot);
+        pane.setLeft(lista);
+
+        // Aseta uusi Scene Stageen ja näytä ikkuna
+        asiakasHallinta.setScene(kehys);
+        asiakasHallinta.show();
+        asiakasHallinta.setTitle("Asiakkaiden hallinta");
+
+        // Luo lisaaAsiakas buttonille toiminto
+        lisaaAsiakas.setOnMouseClicked(e -> {
+            Stage lisaaAsiakasStage = new Stage();
+            GridPane lisaaAsiakasPane = new GridPane();
+            Scene lisaaAsiakasScene = new Scene(lisaaAsiakasPane, 300,300);
+            lisaaAsiakasStage.setScene(lisaaAsiakasScene);
+            lisaaAsiakasStage.show();
+            lisaaAsiakasPane.setVgap(5);
+
+            // Lisää labelit ikkunaan
+            lisaaAsiakasPane.add(new Label("Etunimi: "), 0,0);
+            lisaaAsiakasPane.add(new Label("Sukunimi:"), 0, 1);
+            lisaaAsiakasPane.add(new Label("Lähiosoite: "), 0, 2);
+            lisaaAsiakasPane.add(new Label("Postinumero: "), 0, 3);
+            lisaaAsiakasPane.add(new Label("Sähköposti: "), 0, 4);
+            lisaaAsiakasPane.add(new Label("Puhelinnumero: "), 0, 5);
+
+            // Luo textfieldit
+            TextField etunimi = new TextField();
+            TextField sukunimi = new TextField();
+            TextField lahiosoite = new TextField();
+            TextField postinumero = new TextField();
+            TextField sahkoposti = new TextField();
+            TextField puhelinnumero = new TextField();
+
+            //Luo button
+            Button tallennaAsiakas = new Button("Tallenna");
+
+            // Lisää textfieldit ja button ikkunaan
+            lisaaAsiakasPane.add(etunimi, 1, 0);
+            lisaaAsiakasPane.add(sukunimi, 1,1);
+            lisaaAsiakasPane.add(lahiosoite, 1,2);
+            lisaaAsiakasPane.add(postinumero, 1, 3);
+            lisaaAsiakasPane.add(sahkoposti, 1,4);
+            lisaaAsiakasPane.add(puhelinnumero, 1, 5);
+            lisaaAsiakasPane.add(tallennaAsiakas, 1, 6);
+
+            lisaaAsiakasPane.setAlignment(Pos.CENTER);
+            lisaaAsiakasStage.setTitle("Lisää asiakas");
+
+        });
+
+
+
+
+
+    }
+
+    /**
+     * palveluiden hallinta metodi
+     *
+     */
+    public void palveluidenHallintaIkkuna() {
+        // Luo uusi BorderPane
+        BorderPane pane = new BorderPane();
+        TextArea tiedot = new TextArea();
+        ListView<String> lista = new ListView<>();
+
+        // Luo uusi Stage-olio
+        Stage palveluidenHallinta = new Stage();
+        Scene kehys = new Scene(pane);
+
+        // Luo buttonit ikkunaan
+        Button lisaaPalvelu = new Button("Lisää palvelu");
+        Button muokkaaPalvelua = new Button("Muokkaa palveluita");
+        Button poistaPalvelu = new Button("Poista palvelu");
+
+        // Luo VBox painikkeille ja aseta ne vasemmalle
+        VBox painikeVBox = new VBox(15);
+        painikeVBox.setPadding(new Insets(15, 15, 15, 15));
+        painikeVBox.getChildren().addAll(lisaaPalvelu, muokkaaPalvelua, poistaPalvelu);
+        pane.setRight(painikeVBox);
+
+        // Aseta TextArea ja ListView BorderPaneen
+        pane.setCenter(tiedot);
+        pane.setLeft(lista);
+
+
+        // Aseta uusi Scene Stageen ja näytä ikkuna
+        palveluidenHallinta.setScene(kehys);
+        palveluidenHallinta.show();
+        palveluidenHallinta.setTitle("Palveluiden hallinta");
+
+        // Luo lisaaPalvelu buttonille toiminto
+        lisaaPalvelu.setOnMouseClicked(e -> {
+            Stage lisaaMokkiStage = new Stage();
+            GridPane lisaaMokkiPane = new GridPane();
+            Scene lisaaMokkiScene = new Scene(lisaaMokkiPane, 300,300);
+            lisaaMokkiStage.setScene(lisaaMokkiScene);
+            lisaaMokkiStage.show();
+            lisaaMokkiPane.setVgap(5);
+
+            // Luo labelit
+            lisaaMokkiPane.add(new Label("Palvelun nimi: "), 0,0);
+            lisaaMokkiPane.add(new Label("Hinta:"), 0, 1);
+
+            // Luo textfieldit
+            TextField palvelunNimi = new TextField();
+            TextField hinta = new TextField();
+
+
+            Button tallennaPalvelu = new Button("Tallenna");
+
+            // Lisää tiedot ikkunaan
+            lisaaMokkiPane.add(palvelunNimi, 1,0);
+            lisaaMokkiPane.add(hinta, 1,1);
+            lisaaMokkiPane.add(tallennaPalvelu, 1,2);
+
+            lisaaMokkiPane.setAlignment(Pos.CENTER);
+            lisaaMokkiStage.setTitle("Lisää palvelu");
+
+        });
+    }
+
 
     /**
      * getHbox metodi
